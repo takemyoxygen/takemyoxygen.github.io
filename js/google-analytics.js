34759acfceb20030445361a1a8839f6e873c2326
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2014-01-01 15:18:26 +0300

    Updated Google Analytics key

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,19 +1,17 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-35198518-1']);
-_gaq.push(['_trackPageview']);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+ga('create', 'UA-46789865-1', 'takemyoxygen.github.io');
+ga('send', 'pageview');
   
 $(document).ready(function(){
     $("a").click(function(){
         var linkClass = $(this).attr("class");
         var analyticsEvent = getEventBasedOnLink(linkClass);
         if (analyticsEvent !== null){
-            _gaq.push(['_trackEvent', 'Navigation', analyticsEvent]);
+            ga("send", "event", "Navigation", analyticsEvent)
         }
     });
 });
